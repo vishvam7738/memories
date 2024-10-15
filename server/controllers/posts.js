b3554bd5d5f3d0208e4145d26c840a5c25168438
vishvam <vishvamaggarwal@gmail.com>
2024-10-15 11:59:45 +0530

    Initialization of database

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,9 @@
+import PostMessage from '..models/postMessage.js'
 
-export const getPosts=(req, res) => {
+export const getPosts = (req, res) => {
     res.send("THIS WORKS!")
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation')
 }
