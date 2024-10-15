39a44e6abd4f30d6396c1ec40cd9e5a1a3e13243
vishvam <vishvamaggarwal@gmail.com>
2024-10-15 11:53:54 +0530

    Routes setup

--- server/index.js
+++ server/index.js
@@ -3,3 +3,23 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts',postRoutes);
+
+// Middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// MongoDB connection
+const CONNECTION_URL = "mongodb+srv://vishvam:vishvam123@cluster0.niqjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// No need for 'useFindAndModify' or other deprecated options
