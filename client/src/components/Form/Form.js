a866edbed366b0c6c9cfa45920b5443d93c32cbe
vishvam <vishvamaggarwal@gmail.com>
2024-10-16 14:18:15 +0530

    Added different Form and inputs

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
+import FileBase from 'react-file-base64'
 
 import useStyles from './styles'
 
@@ -23,8 +24,40 @@ const Form = () => {
                     label="Creator"
                     fullWidth
                     value={postData.creator}
-                    onChange={(e) => setPostData({ creator: e.target.value })}
+                    onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
                 />
+                <TextField
+                    name="title"
+                    variant="outlined"
+                    label="Title"
+                    fullWidth
+                    value={postData.title}
+                    onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+                />
+                <TextField
+                    name="message"
+                    variant="outlined"
+                    label="Message"
+                    fullWidth
+                    value={postData.message}
+                    onChange={(e) => setPostData({ ...postData, message: e.target.value })}
+                />
+                <TextField
+                    name="tags"
+                    variant="outlined"
+                    label="Tags"
+                    fullWidth
+                    value={postData.tags}
+                    onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+                />
+
+                <div className={classes.fileInput}>
+                    <FileBase
+                        type="file"
+                        multiple={false}
+                        onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
+                    />
+                </div>
             </form>
         </Paper>
     )
