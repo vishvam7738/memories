8ed046d6f388f7927e73f1af322d05c7243ce033
vishvam <vishvamaggarwal@gmail.com>
2024-10-16 13:50:44 +0530

    Fixed dependencies issues

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post"
 
 import useStyles from './styles'
 
 
 const Posts = () => {
-    const classes=useStyles();
+    const posts = useSelector((state) => state.posts)
+    const classes = useStyles();
+
+    console.log(posts)
     return (
         <>
-        <h1>POSTS</h1>
-        <Post/>
-        <Post/>
+            <h1>POSTS</h1>
+            <Post />
+            <Post />
         </>
     )
 }
