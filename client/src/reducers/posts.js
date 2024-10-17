a9ca50c03ee6d5507c870a7aca29790c8dd1f8e1
vishvam <vishvamaggarwal@gmail.com>
2024-10-17 14:34:32 +0530

    Implemented Card formation

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,11 +1,13 @@
-export default (posts = [], action) => {
+const postsReducer = (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
-            return action.payload;
-        case 'CREATE':
-            return [...posts,action.payload];
-        default:
-            return posts;
+      case 'FETCH_ALL':
+        return action.payload;
+      case 'CREATE':
+        return [...posts, action.payload];
+      default:
+        return posts;
     }
-
-}
+  };
+  
+  export default postsReducer;
+  
