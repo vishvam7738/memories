d5ca70b8a84cda099f090add5da17098302c7d72
vishvam <vishvamaggarwal@gmail.com>
2024-10-17 21:51:44 +0530

    Applied mobile-responsiveness

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -25,7 +25,7 @@ const Post = ({ post, setCurrentId }) => {
                     style={{ color: "white" }}
                     size="small"
                     onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="default" />
+                    <MoreHorizIcon fontSize="medium" />
                 </Button>
             </div>
 
