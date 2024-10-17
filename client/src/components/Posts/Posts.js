edf90f6782f94f104cbfa73810a421a05f854743
vishvam <vishvamaggarwal@gmail.com>
2024-10-17 23:41:59 +0530

    Bug fixes

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -12,16 +12,19 @@ const Posts = ({setCurrentId}) => {
 
     console.log(posts)
     return (
-        !posts.length?<CircularProgress/>:(
-            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post)=>(
-                    <Grid key={post._id} item xs={12} sm={6}>
-                        <Post post={post} setCurrentId={setCurrentId}/>
-                    </Grid>
-                ))}
-            </Grid>
+        !posts || !Array.isArray(posts) || posts.length === 0 ? (
+          <CircularProgress />
+        ) : (
+          <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+            {posts.map((post) => (
+              <Grid key={post._id} item xs={12} sm={6}>
+                <Post post={post} setCurrentId={setCurrentId} />
+              </Grid>
+            ))}
+          </Grid>
         )
-    )
+      );
+    
 }
 
 export default Posts;
