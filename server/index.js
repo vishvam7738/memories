b6d9dd6a4e3eaecf3ce9eb47070de2db3157ec2d
vishvam <vishvamaggarwal@gmail.com>
2024-10-17 22:13:21 +0530

    Added env variables

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express();
-
+dotenv.config()
 
 
 // Middleware
@@ -14,13 +14,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts',postRoutes);
+app.use('/posts', postRoutes);
 
 // MongoDB connection
-const CONNECTION_URL = "mongodb+srv://vishvam:vishvam123@cluster0.niqjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const CONNECTION_URL = "mongodb+srv://vishvam:vishvam123@cluster0.niqjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
