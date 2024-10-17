d12325528e5f49aa3c6d61f838b176c5cf831ee5
vishvam <110680520+vishvam7738@users.noreply.github.com>
2024-10-18 00:15:09 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config()
 
 app.use(cors({
-    origin: ["https://memories-frontend-lemon.vercel.app"],
+    origin: ["https://vishvam-memories.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  
     credentials: true
 }));
