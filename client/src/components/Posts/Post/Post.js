fac603ae55b8e33024ae2974338382b3ab779807
vishvam <vishvamaggarwal@gmail.com>
2024-10-17 14:14:41 +0530

    Setting up of Posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import DeleteIcon from '@material-ui/icons/Delete'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import moment from 'moment'
+
 import useStyles from './styles'
 
-const Post = () => {
-    const classes=useStyles();
+const Post = ({post}) => {
+    const classes = useStyles();
     return (
-        <h1>POST</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+        </Card>
     )
 }
 
