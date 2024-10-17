4c191c056ab1afc141f44236349ca8a4e0ebb6d8
vishvam <110680520+vishvam7738@users.noreply.github.com>
2024-10-17 23:57:30 +0530

    Updated index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-one-theta.vercel.app/?vercelToolbarCode=NoBBKJTJF-LOAHW';
+const url = 'https://memories-one-theta.vercel.app/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
